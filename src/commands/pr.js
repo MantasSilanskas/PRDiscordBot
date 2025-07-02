17b0cf4d6d62e3cc08e734c7ef688894b730e2d1
Mantas Silanskas <mantas.silan@peplink.com>
2025-07-02 14:42:18 +0300

    BE: Error handling and console output update.
    Signed-off-by: Mantas Silanskas <mantas.silan@peplink.com>

--- src/commands/pr.js
+++ src/commands/pr.js
@@ -14,20 +14,50 @@ export async function handlePRCommand(message, client) {
 
   const threadName =
     DateTime.now().setZone(env.timezone).toISODate() + " Pull requests!";
-  const thread = await getOrCreateThread(message, threadName);
-  const existingPRLinks = await getExistingPRLinks(thread);
 
+  let thread;
+  try {
+    thread = await getOrCreateThread(message, threadName);
+  } catch (err) {
+    console.error("PR command failed: ", err);
+    await notifyError(
+      client,
+      env.user_id,
+      err,
+      "Getting or creating thread failed"
+    );
+  }
+
+  let existingPRLinks;
+  try {
+    existingPRLinks = await getExistingPRLinks(thread);
+  } catch (err) {
+    console.error("PR command failed: ", err);
+    await notifyError(
+      client,
+      env.user_id,
+      err,
+      "Fetching existing PR links failed"
+    );
+  }
+
+  let activeCount, wipCount, haltedCount, filteredPRs;
   try {
     const prs = await fetchPullRequests(env.auth_token);
-    const { activeCount, wipCount, haltedCount, filteredPRs } =
-      categorizePRs(prs);
-
-    await postNewPRs(filteredPRs, existingPRLinks, thread, client);
-    logFooter({ activeCount, wipCount, haltedCount });
+    ({ activeCount, wipCount, haltedCount, filteredPRs } = categorizePRs(prs));
   } catch (err) {
     console.error("PR command failed:", err);
-    await notifyError(client, env.user_id, err, "Failed to handle !pr command");
+    await notifyError(client, env.user_id, err, "Failed to fetch PRs");
   }
+
+  try {
+    await postNewPRs(filteredPRs, existingPRLinks, thread, client);
+  } catch (err) {
+    console.error("PR command failed:", err);
+    await notifyError(client, env.user_id, err, "Failed to post new PRs");
+  }
+
+  logFooter({ activeCount, wipCount, haltedCount });
 
   await message.delete();
 }
